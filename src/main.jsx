98040cbd05390ce9469a4d0280a89e7c45c43f5c
Mateo Rodriguez <luismateor73@gmail.com>
2022-12-31 19:23:44 -0500

    Creando el Router y definiendo priemras rutas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import Layout from './components/Layout'
+import Index from './pages/Index'
+import NuevoCliente from './pages/NuevoCliente'
+
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children:[
+      {
+        index: true,
+        element: <Index />
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
