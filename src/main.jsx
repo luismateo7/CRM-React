f4611f1cea6b86d79071b82d86c7ded6462df304
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-08 17:35:56 -0500

    Ruta de editar y eliminar son actions y loaders

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,13 @@ import Layout from './components/Layout'
 import Index from './pages/Index'
 import NuevoCliente from './pages/NuevoCliente'
 import ErrorPage from './components/ErrorPage'
+import EditarCliente from './pages/EditarCliente'
 
 import { loader as clientesLoader } from './pages/Index'
 import { action as nuevoClienteAction } from './pages/NuevoCliente'
+import { loader as editarClienteLoader } from './pages/EditarCliente'
+import { action as editarClienteAction } from './pages/EditarCliente'
+import { action as eliminatClienteAction } from './components/Cliente'
 
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import React from 'react'
@@ -27,6 +31,18 @@ const router = createBrowserRouter([
         path: '/clientes/nuevo',
         element: <NuevoCliente />,
         action: nuevoClienteAction,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/:clienteId/editar',
+        element: <EditarCliente />,
+        loader: editarClienteLoader,
+        action: editarClienteAction,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/:clienteId/eliminar',
+        action: eliminatClienteAction,
         errorElement: <ErrorPage />
       }
     ]
